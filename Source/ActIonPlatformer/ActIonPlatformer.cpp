c6cd925f65867ac59a7be8ddbcb9e00bf0080564
ly0wenka <ly0wenka@gmail.com>
2023-06-27 22:13:33 +0300

    5

---
+++ Source/ActIonPlatformer/ActIonPlatformer.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ActIonPlatformer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ActIonPlatformer, "ActIonPlatformer" );
---
+++ Source/ActIonPlatformer/ActIonPlatformerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActIonPlatformerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONPLATFORMER_API AActIonPlatformerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
